1f7a26e663f27025ef4ca0cf2b55b233b5f3e719
ultragamer1010 <31806444+ultragamer1010@users.noreply.github.com>
2021-03-25 21:01:09 -0400

    Add some movement

--- src/main/java/frc/robot/JsonAutonomous.java
+++ src/main/java/frc/robot/JsonAutonomous.java
@@ -20,12 +20,13 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class JsonAutonomous extends Autonomous{
     private JsonElement auto;
-	private List<AutoInstruction> instructions;
-	private int step;
-	private Timer timer;
-	private double navxStart;
-	private AHRS gyro;
-	private SwerveDrive swerve;
+    private List<AutoInstruction> instructions;
+    private int step;
+    private Timer timer;
+    private double start;
+    private double navxStart;
+    private AHRS gyro;
+    private SwerveDrive swerve;
 
     private FileReader fr;
     private JsonReader jr;
@@ -33,104 +34,131 @@ public class JsonAutonomous extends Autonomous{
 
 
     public static class AutoInstruction
-	{
-		public String type;
-		public Unit unit;
-		public double amount;
-		public List<Double> args;
+    {
+        public String type;
+        public Unit unit;
+        public double amount;
+        public List<Double> args;
 
         
     
         public enum Unit { Seconds, Milliseconds, EncoderTicks, Rotations, Inches, Feet, Degrees, Invalid }
 
-		public AutoInstruction(String type, List<Double> args)
-		{
-			this.type = type;
-			this.args = args;
-		}
+        public AutoInstruction(String type, List<Double> args)
+        {
+            this.type = type;
+            this.args = args;
+        }
 
-		public AutoInstruction(String type, Unit unit, double amount, List<Double> args)
-		{
-			this.type = type;
-			this.unit = unit;
-			this.amount = amount;
-			this.args = args;
-		}
+        public AutoInstruction(String type, Unit unit, double amount, List<Double> args)
+        {
+            this.type = type;
+            this.unit = unit;
+            this.amount = amount;
+            this.args = args;
+        }
     }
 
     /**
-	 * Creates a JsonAutonomous from the specified file
-	 * @param file The location of the file to parse
-	 */
-	public JsonAutonomous(String file, AHRS gyro, SwerveDrive drive) {
-		this.swerve = drive;
-		this.gyro = gyro;
+     * Creates a JsonAutonomous from the specified file
+     * @param file The location of the file to parse
+     */
+    public JsonAutonomous(String file, AHRS gyro, SwerveDrive drive) {
+        this.swerve = drive;
+        this.gyro = gyro;
 
-		parseFile(file);
+        parseFile(file);
     }
 
     public void parseFile(String file)
-	{
-		step = -1;
-		timer = new Timer();
-		instructions = new ArrayList<AutoInstruction>();
-		try
-		{
-			fr = new FileReader(file);
-			jr = new JsonReader(fr);
-			auto = JsonParser.parseReader(jr);
-			JsonElement inner = auto.getAsJsonObject().get("auto");
-			if(inner.isJsonArray())
-			{
-				for(JsonElement e : inner.getAsJsonArray())
-				{
-					JsonObject o = e.getAsJsonObject();
+    {
+        step = -1;
+        timer = new Timer();
+        instructions = new ArrayList<AutoInstruction>();
+        try
+        {
+            fr = new FileReader(file);
+            jr = new JsonReader(fr);
+            auto = JsonParser.parseReader(jr);
+            JsonElement inner = auto.getAsJsonObject().get("auto");
+            if(inner.isJsonArray())
+            {
+                for(JsonElement e : inner.getAsJsonArray())
+                {
+                    JsonObject o = e.getAsJsonObject();
 
-					List<Double> extraArgs = new ArrayList<Double>();
-					for(JsonElement e2 : o.get("args").getAsJsonArray())
-					{
-						extraArgs.add(e2.getAsDouble());
-					}
+                    List<Double> extraArgs = new ArrayList<Double>();
+                    for(JsonElement e2 : o.get("args").getAsJsonArray())
+                    {
+                        extraArgs.add(e2.getAsDouble());
+                    }
 
-					String type = o.get("type").getAsString();
+                    String type = o.get("type").getAsString();
 
-					String unitString = o.has("unit") ? o.get("unit").getAsString() : null;
-					AutoInstruction.Unit unit = unitString != null ? parseUnit(unitString) : null;
+                    String unitString = o.has("unit") ? o.get("unit").getAsString() : null;
+                    AutoInstruction.Unit unit = unitString != null ? parseUnit(unitString) : null;
 
-					double amount = o.has("amount") ? o.get("amount").getAsDouble() : null;
+                    double amount = o.has("amount") ? o.get("amount").getAsDouble() : null;
 
-					AutoInstruction ai = unit == null ? new AutoInstruction(type, extraArgs) : new AutoInstruction(type, unit, amount, extraArgs);
-					instructions.add(ai);
-				}
-			}
-		}
-		catch (JsonIOException | JsonSyntaxException | FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-	}
+                    AutoInstruction ai = unit == null ? new AutoInstruction(type, extraArgs) : new AutoInstruction(type, unit, amount, extraArgs);
+                    instructions.add(ai);
+                }
+            }
+        }
+        catch (JsonIOException | JsonSyntaxException | FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     public static AutoInstruction.Unit parseUnit(String in)
-	{
-		return AutoInstruction.Unit.valueOf(in);
-	}
+    {
+        return AutoInstruction.Unit.valueOf(in);
+    }
 
     @Override
-	public void run(){
+    public void run(){
+    }
+
+    private boolean driveTime(double x, double y, double z, double t, boolean fieldOrient)
+    {
+        if(timer.get() < t)
+        {
+            swerve.swerve(x, y, z, getAngle() + navxStart, fieldOrient);
+        }
+        else
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean driveDistance(double x, double y, double z, double d, boolean fieldOrient)
+    {
+        if(Math.abs(swerve.getEncoder()-start) < d)
+        {
+            swerve.swerve(x, y, z, getAngle() + navxStart, fieldOrient);
+        }
+        else
+        {
+            return true;
+        }
+        return false;
     }
 
     private double getAngle(){
-		return gyro.getAngle();
-	}
+        return gyro.getAngle();
+    }
 
-	private void reset()
-	{
-		step++;
-		swerve.swerve(0.0, 0.0, 0.0, gyro.getAngle(), false);
-		timer.reset();
-		timer.start();
-		gyro.reset();
-		navxStart = getAngle();
-	}
+    private void reset()
+    {
+        step++;
+        swerve.swerve(0.0, 0.0, 0.0, gyro.getAngle(), false);
+        timer.reset();
+        timer.start();
+        gyro.reset();
+        start = swerve.getEncoder();
+        navxStart = getAngle();
+    }
 
 }
--- src/main/java/frc/robot/SwerveDrive.java
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -7,10 +7,10 @@ public class SwerveDrive {
     private double TurningSpeedFactor;
     private double length, width, diameter;
     private double temp;
-	private double a,b,c,d;
-	private double ws1,ws2,ws3,ws4;
-	private double wa1,wa2,wa3,wa4;
-	private double max;
+    private double a,b,c,d;
+    private double ws1,ws2,ws3,ws4;
+    private double wa1,wa2,wa3,wa4;
+    private double max;
     private int movecount;
 
     private SwerveModule FR;
@@ -28,11 +28,11 @@ public class SwerveDrive {
         width = 1;
         diameter = Math.sqrt(Math.pow(length,2)+Math.pow(width,2));
         temp = 0.0;
-		a = 0.0;b = 0.0;c = 0.0;d = 0.0;
-		ws1 = 0.0;ws2 = 0.0;ws3 = 0.0;ws4 = 0.0;
-		wa1 = 0.0;wa2 = 0.0;wa3 = 0.0;wa4 = 0.0;
-		max = 0.0;
-		movecount = 20;
+        a = 0.0;b = 0.0;c = 0.0;d = 0.0;
+        ws1 = 0.0;ws2 = 0.0;ws3 = 0.0;ws4 = 0.0;
+        wa1 = 0.0;wa2 = 0.0;wa3 = 0.0;wa4 = 0.0;
+        max = 0.0;
+        movecount = 20;
     }
 
     // Test Code
@@ -56,90 +56,90 @@ public class SwerveDrive {
     }
 
     public void driverSwerve(double x, double y, double z, double gyro, boolean fieldOrient)
-	{
-		if((x!=0 || y!=0) || z!=0)
-		{
-			swerve(x, y, z, gyro, fieldOrient);
-			movecount = 20;
-		}
-		else
-		{
-			movecount--;
-			if(movecount < 0)
-			{
-				swerve(0, 0, 0, 0, false);
-			}
-			else
-			{
-				FR.setDrive(0);
-				// FL.setDrive(0);
-				// BR.setDrive(0);
-				// BL.setDrive(0);
-			}
-		}
-	}
-	
-	public void swerve(double x, double y, double z, double gyro, boolean fieldOrient)
-	{
-		gyro *= Math.PI/180.0f;
-		z *= TurningSpeedFactor;
-		if(fieldOrient)
-		{
-			temp = y * Math.cos(gyro) + x * Math.sin(gyro);
-			x = -y * Math.sin(gyro) + x * Math.cos(gyro);
-			y = temp;
-		}
+    {
+        if((x!=0 || y!=0) || z!=0)
+        {
+            swerve(x, y, z, gyro, fieldOrient);
+            movecount = 20;
+        }
+        else
+        {
+            movecount--;
+            if(movecount < 0)
+            {
+                swerve(0, 0, 0, 0, false);
+            }
+            else
+            {
+                FR.setDrive(0);
+                // FL.setDrive(0);
+                // BR.setDrive(0);
+                // BL.setDrive(0);
+            }
+        }
+    }
+    
+    public void swerve(double x, double y, double z, double gyro, boolean fieldOrient)
+    {
+        gyro *= Math.PI/180.0f;
+        z *= TurningSpeedFactor;
+        if(fieldOrient)
+        {
+            temp = y * Math.cos(gyro) + x * Math.sin(gyro);
+            x = -y * Math.sin(gyro) + x * Math.cos(gyro);
+            y = temp;
+        }
 
-		a = x - z * (length/diameter);
-		b = x + z * (length/diameter);
-		c = y - z * (width/diameter);
-		d = y + z * (width/diameter);
+        a = x - z * (length/diameter);
+        b = x + z * (length/diameter);
+        c = y - z * (width/diameter);
+        d = y + z * (width/diameter);
 
-		ws1 = Math.sqrt(Math.pow(b,2) + Math.pow(c,2));
-		ws2 = Math.sqrt(Math.pow(b,2) + Math.pow(d,2));
-		ws3 = Math.sqrt(Math.pow(a,2) + Math.pow(d,2));
-		ws4 = Math.sqrt(Math.pow(a,2) + Math.pow(c,2));
-		max = 0;
-		if(ws1 > max){max = ws1;}
-		if(ws2 > max){max = ws2;}
-		if(ws3 > max){max = ws3;}
-		if(ws4 > max){max = ws4;}
-		if(max > 1){ws1 /= max;ws2 /= max;ws3 /= max;ws4 /= max;}
+        ws1 = Math.sqrt(Math.pow(b,2) + Math.pow(c,2));
+        ws2 = Math.sqrt(Math.pow(b,2) + Math.pow(d,2));
+        ws3 = Math.sqrt(Math.pow(a,2) + Math.pow(d,2));
+        ws4 = Math.sqrt(Math.pow(a,2) + Math.pow(c,2));
+        max = 0;
+        if(ws1 > max){max = ws1;}
+        if(ws2 > max){max = ws2;}
+        if(ws3 > max){max = ws3;}
+        if(ws4 > max){max = ws4;}
+        if(max > 1){ws1 /= max;ws2 /= max;ws3 /= max;ws4 /= max;}
 
-		wa1 = Math.atan2(b,c) * 180.0f/Math.PI;
-		wa2 = Math.atan2(b,d) * 180.0f/Math.PI;
-		wa3 = Math.atan2(a,d) * 180.0f/Math.PI;
-		wa4 = Math.atan2(a,c) * 180.0f/Math.PI;
-		
-		if(wa1 < 0){wa1 += 360;}//wa1 = FL
-		if(wa2 < 0){wa2 += 360;}//wa2 = FR
-		if(wa3 < 0){wa3 += 360;}//wa3 = BR
-		if(wa4 < 0){wa4 += 360;}//wa4 = BL
+        wa1 = Math.atan2(b,c) * 180.0f/Math.PI;
+        wa2 = Math.atan2(b,d) * 180.0f/Math.PI;
+        wa3 = Math.atan2(a,d) * 180.0f/Math.PI;
+        wa4 = Math.atan2(a,c) * 180.0f/Math.PI;
+        
+        if(wa1 < 0){wa1 += 360;}//wa1 = FL
+        if(wa2 < 0){wa2 += 360;}//wa2 = FR
+        if(wa3 < 0){wa3 += 360;}//wa3 = BR
+        if(wa4 < 0){wa4 += 360;}//wa4 = BL
 
-		double joystickA = wa2;
+        double joystickA = wa2;
 
         SwerveTarget tmp;
         // if((tmp = closestAngle(FR.getAngleEncoder()/FR.getEncPerDeg(), wa2)) != wa2 && ((tmp + 360)%360 != wa2 || (tmp - 360)%360 != wa2))
         // {
-		// 	System.out.println("FLIPPED");
+        // 	System.out.println("FLIPPED");
         //     wa2 = tmp;
         //     ws2 *= -1;
         // }
 
-		double currentAngle = FR.getAngleEncoder() / FR.getEncPerDeg();
+        double currentAngle = FR.getAngleEncoder() / FR.getEncPerDeg();
 
-		System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, wa2, currentAngle));
+        System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, wa2, currentAngle));
 
-		// System.out.println(String.format("A: %3.2f", wa2));
+        // System.out.println(String.format("A: %3.2f", wa2));
         tmp = closestAngle(currentAngle, wa2);
-		// System.out.println(String.format("A: %3.2f", wa2));
-		// System.out.println("-------------");
+        // System.out.println(String.format("A: %3.2f", wa2));
+        // System.out.println("-------------");
 
-		System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, tmp.getTarget(), currentAngle));
+        System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, tmp.getTarget(), currentAngle));
 
-		wa2 = tmp.getTarget();
-		ws2 *= tmp.getMotorScale();
-		
+        wa2 = tmp.getTarget();
+        ws2 *= tmp.getMotorScale();
+        
         // if((tmp = closestAngle(FL.getAngleEncoder()/FL.getEncPerDeg(), wa1)) != wa1)
         // {
         //     wa1 = tmp;
@@ -157,103 +157,107 @@ public class SwerveDrive {
         // }
 
 
-		System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, wa2, currentAngle));
-		System.out.println("-------------");
+        System.out.println(String.format("J: %3.2f \tA: %3.2f \t P: %3.2f", joystickA, wa2, currentAngle));
+        System.out.println("-------------");
 
-		
-		FR.setDrive(ws2);
-		// FL.setDrive(-ws1);
-		// BR.setDrive(ws3);
-		// BL.setDrive(-ws4);
+        
+        FR.setDrive(ws2);
+        // FL.setDrive(-ws1);
+        // BR.setDrive(ws3);
+        // BL.setDrive(-ws4);
 
-		// System.out.println("J: " + String.valueOf(wa2));
-		// System.out.println("A: " + String.valueOf(wa2));
-		// System.out.println("S: " + String.valueOf(ws2));
-		// System.out.println("");
+        // System.out.println("J: " + String.valueOf(wa2));
+        // System.out.println("A: " + String.valueOf(wa2));
+        // System.out.println("S: " + String.valueOf(ws2));
+        // System.out.println("");
 
-		FR.setAngle(wa2);
-		FL.setAngle(wa1);
-		BR.setAngle(wa3);
-		BL.setAngle(wa4);
-	}
-	
-	public void setBrake()
-	{
-		FR.setBrake();
-		FL.setBrake();
-		BR.setBrake();
-		BL.setBrake();
-	}
-	
-	public void setCoast()
-	{
-		FR.setCoast();
-		FL.setCoast();
-		BR.setCoast();
-		BL.setCoast();
-	}
-	
-	public void angleToZero()
-	{
-		FR.setDrive(0);
-		// FL.setDrive(0);
-		// BR.setDrive(0);
-		// BL.setDrive(0);
-		FR.setAngle(0);
-		FL.setAngle(0);
-		BR.setAngle(0);
-		BL.setAngle(0);
-	}
-	
-	public void tankDrive(double left, double right)
-	{
-		angleToZero();
-		FR.setDriveSpeed(right);
-		// BR.setDriveSpeed(right);
-		// FL.setDriveSpeed(-left);
-		// BL.setDriveSpeed(-left);
-	}
+        FR.setAngle(wa2);
+        FL.setAngle(wa1);
+        BR.setAngle(wa3);
+        BL.setAngle(wa4);
+    }
+    
+    public void setBrake()
+    {
+        FR.setBrake();
+        FL.setBrake();
+        BR.setBrake();
+        BL.setBrake();
+    }
+    
+    public void setCoast()
+    {
+        FR.setCoast();
+        FL.setCoast();
+        BR.setCoast();
+        BL.setCoast();
+    }
+    
+    public void angleToZero()
+    {
+        FR.setDrive(0);
+        // FL.setDrive(0);
+        // BR.setDrive(0);
+        // BL.setDrive(0);
+        FR.setAngle(0);
+        FL.setAngle(0);
+        BR.setAngle(0);
+        BL.setAngle(0);
+    }
+    
+    public void tankDrive(double left, double right)
+    {
+        angleToZero();
+        FR.setDriveSpeed(right);
+        // BR.setDriveSpeed(right);
+        // FL.setDriveSpeed(-left);
+        // BL.setDriveSpeed(-left);
+    }
 
     // public static double closestAngle(double p, double t)
-	// {
-	// 	// return t;
-	// 	System.out.println("Position, Target:" + String.valueOf(p) + ":" + String.valueOf(t));
+    // {
+    // 	// return t;
+    // 	System.out.println("Position, Target:" + String.valueOf(p) + ":" + String.valueOf(t));
 
-	// 	p %= 360;
-	// 	double t1 = t % 360;
-	// 	double t2 = (t1+180)%360;
+    // 	p %= 360;
+    // 	double t1 = t % 360;
+    // 	double t2 = (t1+180)%360;
 
-	// 	double d1 = Math.abs(p - t1);
-	// 	if(d1 > 180) d1 = 360 - d1;
+    // 	double d1 = Math.abs(p - t1);
+    // 	if(d1 > 180) d1 = 360 - d1;
 
-	// 	double d2 = Math.abs(p -t2);
-	// 	if(d2 > 180) d2 = 360 - d2;
+    // 	double d2 = Math.abs(p -t2);
+    // 	if(d2 > 180) d2 = 360 - d2;
 
-	// 	return (d1 < d2 ? t1 : t2);
-	// }
+    // 	return (d1 < d2 ? t1 : t2);
+    // }
 
-	public static SwerveTarget closestAngle(double p, double t)
-	{
-		// return new SwerveTarget(t, 1);
+    public static SwerveTarget closestAngle(double p, double t)
+    {
+        // return new SwerveTarget(t, 1);
 
-		double pTemp = p;
-		
-		p %= 360;
-		if(p < 0) p += 360;
-		
-		double t1 = t % 360;
-		
-		double d1 = t1 - p;
-		if(d1 > 180) d1 = d1 - 360;
-		if(d1 < -180) d1 = d1 + 360;
+        double pTemp = p;
+        
+        p %= 360;
+        if(p < 0) p += 360;
+        
+        double t1 = t % 360;
+        
+        double d1 = t1 - p;
+        if(d1 > 180) d1 = d1 - 360;
+        if(d1 < -180) d1 = d1 + 360;
 
         double d2 = (d1 > 0 ? d1 - 180 : 180 + d1);
-		double df = (Math.abs(d1) <= Math.abs(d2) ? d1 : d2);
-		double motorScale = (Math.abs(d1) <= Math.abs(d2) ? 1 : -1);
+        double df = (Math.abs(d1) <= Math.abs(d2) ? d1 : d2);
+        double motorScale = (Math.abs(d1) <= Math.abs(d2) ? 1 : -1);
 
-		double target = pTemp + df;
+        double target = pTemp + df;
 
-		return new SwerveTarget(target, motorScale);
-	}
+        return new SwerveTarget(target, motorScale);
+    }
+
+    public double getEncoder() {
+        return FR.getDriveEnc();
+    }
 }
 
